a6c379e917af86558cdfe9291a81fc6cd3df50b4
Dan Pickett <dan.pickett@launchacademy.com>
2017-04-22 17:00:37 -0400

    result of setInterval video

--- main.js
+++ main.js
@@ -7,5 +7,17 @@
 - Cancel intervals with the clearInterval function
 */
 
-console.log('Starting the show in 3 seconds...')
-console.log('We are live and broadcasting!')
+// console.log('Starting the show in 5 seconds...')
+//
+// let confirmLive = () => {
+//   console.log('We are live and broadcasting!')
+// }
+// setTimeout(confirmLive, 5000)
+
+let interval = setInterval(() => {
+  console.log('2 seconds have elapsed')
+}, 2000)
+
+setTimeout(() => {
+  clearInterval(interval)
+}, 7000)
