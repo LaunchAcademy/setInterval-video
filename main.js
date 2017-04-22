4fb02f556c1305e7d1f1fc4ab923193abe07ba2f
Dan Pickett <dan.pickett@launchacademy.com>
2017-04-22 16:41:17 -0400

    video start

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+# Learning Objectives
+
+- Use anonymous functions with setTimeout
+- Use declared functions with setTimeout
+- Use functions with setInterval
+- Cancel intervals with the clearInterval function
+*/
+
+console.log('Starting the show in 3 seconds...')
+console.log('We are live and broadcasting!')
